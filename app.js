420f2e45414b3fcc219758a748d3b5b1fa5b68c6
Lucas Anjos <anjoslucas52@gmail.com>
2022-09-02 16:05:16 -0300

    :hammer: Refatorando o Código e modularizando as rotas

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const app = require('./config/server')
+const rotaHome = require('./app/routes/home')
+const rotaNoticias = require('./app/routes/noticias')
+const rotaformularioInclusao = require('./app/routes/formulario_inclusao_noticia')
 
-
-app.get('/', function(req, res){
-    res.render('home/index')
-})
-
-app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render('admin/form_add_noticia')
-})
-
-app.get('/noticias', function(req, res){
-    res.render('noticias/noticias')
-})
-
+rotaHome(app)
+rotaNoticias(app)
+rotaformularioInclusao(app)
 
 app.listen(3000, function(){
     console.log('🚀 Server Runing')
